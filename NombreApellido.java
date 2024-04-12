1d1a00e7bdcf7025d3b4a415f2eb9cf07312ae67
jaime <jaialbjai@gmail.com>
2024-04-12 08:38:52 +0200

    He modificado el fichero .java

--- NombreApellido.java
+++ NombreApellido.java
@@ -1,1 +1,7 @@
 // Hola soy Jaime Alberto
+public class NombreApellido {
+    public static void main(String[] args) {
+        String nombre = "Jaime Alberto";
+        System.out.println("¡Hola, " + nombre + "!");
+    }
+}
